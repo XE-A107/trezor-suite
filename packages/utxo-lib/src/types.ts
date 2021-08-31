d43da92c8bb448bb746c934b477a0431b270791d
Szymon Lesisz <sz.lesisz@gmail.com>
2021-08-31 17:13:03 +0200

    feat(utxo-lib): add types module

---
+++ packages/utxo-lib/src/types.ts
@@ -0,0 +1,27 @@
+import * as typeforce from 'typeforce';
+
+const SATOSHI_MAX = 21 * 1e14;
+export function Satoshi(value: number) {
+    return typeforce.UInt53(value) && value <= SATOSHI_MAX;
+}
+
+export const Buffer256bit = typeforce.BufferN(32);
+export const Hash160bit = typeforce.BufferN(20);
+export const Hash256bit = typeforce.BufferN(32);
+export const {
+    Number,
+    Array,
+    Boolean,
+    String,
+    Buffer,
+    Hex,
+    maybe,
+    tuple,
+    UInt8,
+    UInt16,
+    UInt32,
+    Function,
+    BufferN,
+    Nil,
+    anyOf,
+} = typeforce;
